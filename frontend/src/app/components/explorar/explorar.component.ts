1545c6258a0201674d75393536ffedb3fc4187af
Nando Soares <barsoaf@gmail.com>
2024-10-23 10:41:37 -0300

    Feat: Added the Truncate function for limiting text length

--- frontend/src/app/components/explorar/explorar.component.ts
+++ frontend/src/app/components/explorar/explorar.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './explorar.component.scss'
 })
 export class ExplorarComponent {
+  userDescription: string = 'Sou Alina Dias, estudante tranquila, nas horas vagas gosto de ler e estudar.';
+  
   tags: { name: string; color?: string }[] = [
     { name: 'Estudos' },
     { name: 'Tecnologia', color: 'rgb(133, 135, 150)' },
@@ -16,5 +18,22 @@ export class ExplorarComponent {
     { name: 'Idiomas' },
     { name: 'Voluntariado' },
     { name: 'Organização' },
+    { name: 'Viagens' },
   ];
+
+  truncate(value: string, limit: number = 65, completeWords: boolean = false, ellipsis: string = '...'): string {
+    if (!value || typeof value !== 'string') return '';
+    if (value.length <= limit) return value;
+
+    let truncated = value.slice(0, limit);
+
+    if (completeWords) {
+      const lastSpaceIndex = truncated.lastIndexOf(' ');
+      if (lastSpaceIndex > -1) {
+        truncated = truncated.slice(0, lastSpaceIndex);
+      }
+    }
+
+    return truncated + ellipsis;
+  }
 }
