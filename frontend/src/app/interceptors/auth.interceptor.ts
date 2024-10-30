3c83eee95bdb56f50fdaaf0ab5127a8d33f92d10
Pedro Athayde <pedro_athayde@outlook.com>
2024-10-30 09:41:09 -0300

    chore: cleaned console logs from auth interceptor

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -12,20 +12,16 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Intercepted request:', req);
-
+   
     // Clone the request to add a custom header (e.g., Authorization)
     const clonedRequest = req.clone({
       headers: req.headers.set('Authorization', 'Bearer dummy-token'),
     });
 
-    console.log('Modified request:', clonedRequest);
-
+  
     // Pass the cloned request to the next handler and log the response
     return next.handle(clonedRequest).pipe(
       tap(
-        (event) => console.log('Response received:', event),
-        (error) => console.error('Request error:', error)
       )
     );
   }
