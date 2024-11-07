1b1b12dfd9a602159d29faca84cd8fcd6c505088
Pedro Athayde <pedro_athayde@outlook.com>
2024-11-07 16:12:58 -0300

    feat: added initial form validation

--- frontend/src/app/components/auth/register-owner/register-owner.component.ts
+++ frontend/src/app/components/auth/register-owner/register-owner.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
@@ -26,13 +26,58 @@ export class RegisterOwnerComponent {
 
   ngOnInit(): void {
     this.ownerForm = this.fb.group({
+      username: ['', Validators.required],
       identifier: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+      gender: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      
     });
 
     const today = new Date();
     const minAgeDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
     this.maxDate = minAgeDate.toISOString().split('T')[0];
   }
+
+  onRegistrarClick(){
+    if(this.ownerForm.valid){
+      console.log(this.ownerForm.value);
+    }
+    console.log("Form not valid");
+    
+  }
+
+  //getters
+
+  get username() {
+    return this.ownerForm.get('username');
+  }
+
+  get identifier() {
+    return this.ownerForm.get('identifier');
+  }
+
+  get password() {
+    return this.ownerForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.ownerForm.get('confirmPassword');
+  }
+
+  get gender() {
+    return this.ownerForm.get('gender');
+  }
+
+  get birthDate() {
+    return this.ownerForm.get('birthDate');
+  }
+
+  get phoneNumber() {
+    return this.ownerForm.get('phoneNumber');
+  }
+  
 
 }
