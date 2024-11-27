079673e51b9490819ed211507c4313be0f56f57c
Joao Henrique <joaoadsistemas@gmail.com>
2024-11-26 21:57:07 -0300

    feat: findAll properties

--- backend/src/main/java/com/alugaai/backend/controllers/PropertyController.java
+++ backend/src/main/java/com/alugaai/backend/controllers/PropertyController.java
@@ -1,15 +1,15 @@
 package com.alugaai.backend.controllers;
 
 import com.alugaai.backend.dtos.property.PropertyRequestDTO;
+import com.alugaai.backend.dtos.property.PropertyResponseDTO;
 import com.alugaai.backend.models.Owner;
 import com.alugaai.backend.services.PropertyService;
 import com.alugaai.backend.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/property")
@@ -24,6 +24,11 @@ public class PropertyController {
        var user = userService.getCurrentUser();
        propertyService.post(request, (Owner) user);
        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<PropertyResponseDTO>> findAll() {
+        return ResponseEntity.ok(propertyService.listAll());
     }
 
 }
