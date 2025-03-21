4aa7ffb08ec5278fcbef46f14ce8950b4cf76184
Pedro Athayde <pedro_athayde@outlook.com>
2025-03-21 15:15:18 -0300

    feat: add initial navigation

---
+++ mobile/lib/utils/Colors.dart
@@ -0,0 +1,10 @@
+// lib/constants/colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Orange Color
+  static const Color primaryOrangeColor = Color(0xFFFD853A);
+
+  // Primary Text Color
+  static const Color primaryTextColor = Color(0xFF465467);
+}
